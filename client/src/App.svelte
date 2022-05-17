aa89f81bb7b534dd7601bad1a836b3b885ad8727
FalkaiPeter <formal.pfalkai@gmail.com>
2022-05-17 18:47:35 +0200

    feat(client): router and pages added

---
+++ client/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Game, GameCreator, GameLobby, Landing, NotFound } from 'routes';
+  import { cards } from 'stores';
+  import Router from 'svelte-spa-router';
+
+  const routes = {
+    '/': Landing,
+    '/lobby/:roomID': GameLobby,
+    '/game/:roomID': Game,
+    '/create': GameCreator,
+    '*': NotFound,
+  };
+</script>
+
+{#await cards.load()}
+  loading...
+{:then _}
+  <Router {routes} />
+{/await}
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,1 +1,5 @@
-
+export { default as Game } from './_Game.svelte';
+export { default as GameCreator } from './_GameCreator.svelte';
+export { default as GameLobby } from './_GameLobby.svelte';
+export { default as Landing } from './_Landing.svelte';
+export { default as NotFound } from './_NotFound.svelte';
