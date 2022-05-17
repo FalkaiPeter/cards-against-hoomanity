b26898066fdca3f3a3476113c235d1562841f3a8
FalkaiPeter <formal.pfalkai@gmail.com>
2022-05-17 17:48:39 +0200

    feat(client): types

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,28 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+type BlackCard = { text: string; pick: number };
+type Pack = { name: string; black: number[]; white: number[]; offical: boolean };
+interface Cards {
+  black: BlackCard[];
+  white: string[];
+  packs: Pack[];
+}
+
+interface GameRoom {
+  id: string;
+  czar: string;
+  owner: string;
+  black: number;
+  packs: number[];
+  state: 'running' | 'lobby';
+  players: Record<string, Player>;
+}
+
+export interface Player {
+  id: string;
+  name: string;
+  points: number;
+  cards: number[];
+  picks: number[];
+}
