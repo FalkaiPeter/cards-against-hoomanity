657a467c0f9f74a6aa955a87787512771e46941a
FalkaiPeter <formal.pfalkai@gmail.com>
2022-05-17 17:37:12 +0200

    feat(server): clock type

---
+++ server/src/types/_clock.ts
@@ -0,0 +1,16 @@
+import { uuid } from 'utils';
+
+export interface Clock {
+  id: string;
+  counter: number;
+  cb: (counter: number) => void;
+  interval: NodeJS.Timer;
+}
+
+export const Clock = (fields?: Partial<Clock>): Clock => ({
+  id: uuid(),
+  cb: () => {},
+  counter: -1,
+  interval: setInterval(() => {}, Infinity),
+  ...fields,
+});
